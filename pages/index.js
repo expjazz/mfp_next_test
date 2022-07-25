d1bae3de91549c2dfd199ff21c8950f29f1e033b
Expedito Andrade <expeditojazz@gmail.com>
2022-07-25 14:18:26 -0300

    syntax

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
